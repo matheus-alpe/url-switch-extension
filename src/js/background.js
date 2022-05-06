2e1751d81d7e47af0c8a678511183c471089ac1f
Matheus Pereira <matttalves@gmail.com>
2022-05-06 00:39:10 -0300

    feat: onBeforeRequest, typedef Rule

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,28 @@
 /**
  * Todo:
- *  [] Listen to request
- *  [] Rules definition
- *  [] CRUD of rules
- *  [] Create a function that handle request redirect
+ *  [x] Listen to request
+ *  [x] Rules definition
+ *  [ ] CRUD of rules
+ *  [ ] Create a function that handle request redirect
  */
+
+/**
+ * @typedef {Object} Rule
+ * @property {string} from - Specific URL
+ * @property {string} to - Specific URL
+ * @property {boolean} active - Determines whether the rule is active
+ */
+
+const requestHandler = function(details) {
+    console.log(details);
+};
+
+
+const RequestConfig = {
+    filter: {
+        urls: ['<all_urls>']
+    },
+    opt_extraInfoSpec: ['blocking']
+}
+
+chrome.webRequest.onBeforeRequest.addListener(requestHandler, RequestConfig.filter, RequestConfig.opt_extraInfoSpec);
