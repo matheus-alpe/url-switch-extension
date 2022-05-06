95e98316b215950de3f81a5930a4275ed15c8c23
Matheus Pereira <matttalves@gmail.com>
2022-05-06 01:03:23 -0300

    feat: create first redirect, fix identention with prettier

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Switch - A simple extension that redirect requests.
 
-- Working with [Manifest V2](https://developer.chrome.com/docs/extensions/mv2/manifest/)
+-   Working with [Manifest V2](https://developer.chrome.com/docs/extensions/mv2/manifest/)
--- src/js/background.js
+++ src/js/background.js
@@ -12,17 +12,30 @@
  * @property {string} to - Specific URL
  * @property {boolean} active - Determines whether the rule is active
  */
-
-const requestHandler = function(details) {
-    console.log(details);
+const rule = {
+    from: '//www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+    to: '//i.pinimg.com/originals/78/cf/3e/78cf3eee0658dbf205e821f5a31db5e3.png',
+    active: true,
 };
 
+const requestHandler = function (details) {
+    // test on https://www.google.com.br/
+    if (rule.active && details.url.includes(rule.from)) {
+        return {
+            redirectUrl: details.url.replace(rule.from, rule.to),
+        };
+    }
+};
 
 const RequestConfig = {
     filter: {
-        urls: ['<all_urls>']
+        urls: ['<all_urls>'],
     },
-    opt_extraInfoSpec: ['blocking']
-}
+    opt_extraInfoSpec: ['blocking'],
+};
 
-chrome.webRequest.onBeforeRequest.addListener(requestHandler, RequestConfig.filter, RequestConfig.opt_extraInfoSpec);
+chrome.webRequest.onBeforeRequest.addListener(
+    requestHandler,
+    RequestConfig.filter,
+    RequestConfig.opt_extraInfoSpec
+);
