f788c2d057333da85790b5c45aeba5821ef36888
Matheus Pereira <matttalves@gmail.com>
2022-05-08 16:04:47 -0300

    feat: new webpack config, new structure

--- src/js/popup.js
+++ src/js/popup.js
@@ -10,11 +10,10 @@ const inputTo = document.querySelector('#to');
 
 const submitBtn = document.querySelector('#submit');
 
-
 storage.get('rules').then((response) => {
     rules = response.rules;
     refreshRules();
-})
+});
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -22,16 +21,16 @@ submitBtn.addEventListener('click', (e) => {
     let to = inputTo.value;
 
     let newRule = {
-        "from": from,
-        "to": to,
+        from: from,
+        to: to,
         active: true,
-        isEditing: false
-    }
+        isEditing: false,
+    };
 
     rules.push(newRule);
     updateRules();
     refreshRules();
-})
+});
 
 function updateRules() {
     storage.set('rules', rules);
@@ -41,22 +40,27 @@ function updateRules() {
 function refreshRules() {
     rulesUl.innerHTML = '';
 
-    if(rules.length) {
+    if (rules.length) {
         for (var i = 0; i < rules.length; i++) {
             var rule = rules[i];
             var checked = rule.active ? 'checked="checked"' : '';
-            var li = 
-            `
-                <li class="${rule.active ? 'active' : ''}" data-rule-index="${i}">
-                    <input class="from" title="${rule.from}" ${rule.isEditing ? '' : 'disabled'} value="${rule.from}" />
+            var li = `
+                <li class="${
+                    rule.active ? 'active' : ''
+                }" data-rule-index="${i}">
+                    <input class="from" title="${rule.from}" ${
+                rule.isEditing ? '' : 'disabled'
+            } value="${rule.from}" />
                     <span class="seperator">&gt;</span>
-                    <input class="to" title="${rule.to}" ${rule.isEditing ? '' : 'disabled'} value="${rule.to}" />
+                    <input class="to" title="${rule.to}" ${
+                rule.isEditing ? '' : 'disabled'
+            } value="${rule.to}" />
                     <input type="checkbox" class="checkbox" name="active" ${checked} />
                     <a href="#" class="editRuleButton" >Edit</a>
                     <a href="#" class="removeRuleButton">Remove</a>
                 </li>
             `;
-            
+
             rulesUl.appendChild(stringToHTML(li));
         }
 
@@ -64,11 +68,11 @@ function refreshRules() {
     }
 }
 
-function stringToHTML (str) {
-	var parser = new DOMParser();
-	var doc = parser.parseFromString(str, 'text/html');
-	return doc.body.firstElementChild;
-};
+function stringToHTML(str) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(str, 'text/html');
+    return doc.body.firstElementChild;
+}
 
 function bindEventActions() {
     // Edit
@@ -79,8 +83,8 @@ function bindEventActions() {
         updateRules();
     }
 
-    var editButtons =  rulesUl.querySelectorAll('.editRuleButton');
-    editButtons.forEach( (btn) => {
+    var editButtons = rulesUl.querySelectorAll('.editRuleButton');
+    editButtons.forEach((btn) => {
         btn.removeEventListener('click', clickEditAction);
         btn.addEventListener('click', clickEditAction);
     });
@@ -92,8 +96,8 @@ function bindEventActions() {
         rules.splice(index, 1);
         updateRules();
     }
-    var removeButtons =  rulesUl.querySelectorAll('.removeRuleButton');
-    removeButtons.forEach( (btn) => {
+    var removeButtons = rulesUl.querySelectorAll('.removeRuleButton');
+    removeButtons.forEach((btn) => {
         btn.removeEventListener('click', clickRemoveAction);
         btn.addEventListener('click', clickRemoveAction);
     });
@@ -105,8 +109,8 @@ function bindEventActions() {
         rules[index].active = e.target.checked;
         updateRules();
     }
-    var checkboxButtons =  rulesUl.querySelectorAll('.checkbox');
-    checkboxButtons.forEach( (btn) => {
+    var checkboxButtons = rulesUl.querySelectorAll('.checkbox');
+    checkboxButtons.forEach((btn) => {
         btn.removeEventListener('change', toggleCheckbox);
         btn.addEventListener('change', toggleCheckbox);
     });
