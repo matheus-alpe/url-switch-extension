4ae38285c6b33ce8ccb02431a6e4e1423d89659a
Matheus Alves Pereira <matt.alpe.dev@gmail.com>
2024-04-03 15:07:15 -0300

    feat: vuetify

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,65 @@
 import { ref, onBeforeMount } from 'vue'
 import { storage, closeView } from './utils'
 
-const KEY = 'count'
-const count = ref(0)
+const KEY = 'rules'
+const rules = ref<Object[]>([])
 
-async function increment() {
-  count.value++
-  await storage.set(KEY, count.value)
+async function save() {
+  // TODO: implement save rules
+  await storage.set(KEY, rules.value)
 }
 
 onBeforeMount(async () => {
-  const value = await storage.get<number>(KEY)
-  count.value = value || 0
+  const value = await storage.get<Object[]>(KEY)
+  rules.value = value || []
 })
 </script>
 
 <template>
   <div class="container">
-    <p @click="closeView">close</p>
+    <VBtn
+      icon="mdi-close"
+      variant="plain"
+      @click="closeView"
+    />
 
-    Counter: <button @click="increment">{{ count }}</button>
+    <VForm>
+      <VContainer>
+        <VRow class="input-row">
+          <VCol cols="6">
+            <VTextField
+              label="original url"
+              required
+            />
+          </VCol>
+
+          <VIcon
+            class="sep"
+            icon="mdi-chevron-right"
+          />
+
+          <VCol cols="6">
+            <VTextField
+              label="redirect to"
+              required
+            />
+          </VCol>
+        </VRow>
+      </VContainer>
+    </VForm>
   </div>
 </template>
+
+<style lang="scss">
+.input-row {
+  position: relative;
+
+  .v-icon {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 30px;
+    opacity: 0.5;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+import '@mdi/font/css/materialdesignicons.css'
+import './style.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
